ab7fa339c7f3ca3ba6fc64167db02f909919a9ee
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2019-01-07 16:48:31 +0100

    initial commit

---
+++ R/theme_janco.R
@@ -0,0 +1,94 @@
+#' Minimal theme for scatterplots (etc)
+#'
+#' @description A minimal theme based on ggthemes::theme_fivethirtyeight()
+#'     This theme has both horizontal and vertical panel.grids
+#' @param base_size global font size (number)
+#' @param base_family global font family (e.g. sans, serif)
+#' @return Adds a theme to a ggplot
+#' @examples
+#' ggplot(mtcars, aes(x = wt, y = mpg, colour = factor(gear))) +
+#' geom_point() +
+#' theme_janco_point()
+
+theme_janco_point <- function(base_size = 12, base_family = "sans") {
+
+  ## theme specs
+  (theme_foundation(base_size = base_size, base_family = base_family)
+    + theme(
+      line = element_line(colour = "black"),
+      rect = element_rect(fill = "#F0F0F0",
+                          linetype = 0, colour = NA),
+      text = element_text(colour = "#3C3C3C"),
+
+      # AXIS
+      ## axis labs
+      axis.title.x = element_text(margin = margin(t = 10)),
+      axis.title.y = element_text(margin = margin(r = 15)),
+      ## axis other
+      axis.text = element_text(),
+      axis.ticks = element_blank(),
+      axis.line = element_blank(),
+
+      # LEGEND
+      legend.background = element_rect(),
+      legend.position = "bottom",
+      legend.direction = "horizontal",
+      legend.box = "vertical",
+
+      # GRID AND TITLE
+      panel.grid = element_line(colour = NULL),
+      panel.grid.major =
+        element_line(colour = colors["Medium Gray"]),
+      panel.grid.minor = element_blank(),
+      plot.title = element_text(hjust = 0, size = rel(1.5), face = "bold"),
+      plot.margin = unit(c(1, 1, 1, 1), "lines"),
+      strip.background = element_rect()))
+}
+
+#' Minimal theme for barplots (etc)
+#'
+#' @description A minimal theme based on ggthemes::theme_fivethirtyeight()
+#'     This theme has no vertical panel.grid
+#' @param base_size global font size (number)
+#' @param base_family global font family (e.g. sans, serif)
+#' @return Adds a theme to a ggplot
+#' @examples
+#' ggplot(mtcars, aes(x = wt, y = mpg, colour = factor(gear))) +
+#'   geom_point() +
+#'   theme_janco_bar()
+
+
+theme_janco_bar <- function(base_size = 12, base_family = "sans") {
+
+  ## theme specs
+  (theme_foundation(base_size = base_size, base_family = base_family)
+   + theme(
+     line = element_line(colour = "black"),
+     rect = element_rect(fill = "#F0F0F0",
+                         linetype = 0, colour = NA),
+     text = element_text(colour = "#3C3C3C"),
+
+     # AXIS
+     ## axis labs
+     axis.title.x = element_text(margin = margin(t = 10)),
+     axis.title.y = element_text(margin = margin(r = 15)),
+     ## axis other
+     axis.text = element_text(),
+     axis.ticks = element_blank(),
+     axis.line = element_blank(),
+
+     # LEGEND
+     legend.background = element_rect(),
+     legend.position = "bottom",
+     legend.direction = "horizontal",
+     legend.box = "vertical",
+
+     # GRID AND TITLE
+     panel.grid = element_line(colour = NULL),
+     panel.grid.major = element_line(colour = colors["Medium Gray"]),
+     panel.grid.minor = element_blank(),
+     panel.grid.major.x = element_blank(),
+     plot.title = element_text(hjust = 0, size = rel(1.5), face = "bold"),
+     plot.margin = unit(c(1, 1, 1, 1), "lines"),
+     strip.background = element_rect()))
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(ggunchained)
+
+test_check("ggunchained")
